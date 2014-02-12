a2cda2afd494b200a0fe59a01b315853f35506c5
Pierre Radermecker <pradermecker@cirb.irisnet.be>
2014-02-12 19:06:09 +0100

    Polish code

--- saltevent.hs
+++ saltevent.hs
@@ -1,34 +1,41 @@
-{-# LANGUAGE OverloadedStrings, NoMonomorphismRestriction, DeriveGeneric #-}
+{-# LANGUAGE DeriveGeneric             #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE OverloadedStrings         #-}
 module Main
 where
 
-import Control.Applicative
-import Control.Monad
-import Control.Lens(view)
-import Data.Aeson
-import Data.ByteString (ByteString)
+import           Control.Applicative
+import           Control.Lens        (view)
+import           Control.Monad
+import           Data.Aeson
+import           Data.ByteString     (ByteString)
 --import Database.PostgreSQL.Simple
-import GHC.Generics
-import Pipes
-import qualified Pipes.Aeson as PAe
-import Pipes.HTTP
-import Pipes.Group
-import Pipes.Parse
-import qualified Pipes.ByteString as PB
+import           GHC.Generics
+import           Pipes
+import qualified Pipes.Aeson         as PAe
+import qualified Pipes.ByteString    as PB
+import           Pipes.Group
+import           Pipes.HTTP
+import           Pipes.Parse
 
+-- Removing "data: " by brute force for now
+jsonLowerBound :: Int
+jsonLowerBound = 6
+
+-- JSON PARSING
 data Event = Event {
-    tag :: String
+    tag     :: String
     , _data :: Command
 } deriving Show
 
 data Command = Command {
-    jid :: String
-    , user :: String
-    , _stamp :: String
-    , tgt :: String
+    jid       :: String
+    , user    :: String
+    , _stamp  :: String
+    , tgt     :: String
     , minions :: [String]
-    , fun :: String
-    , arg :: [Value]
+    , fun     :: String
+    , arg     :: [Value]
 } deriving (Show, Generic)
 
 instance FromJSON Command
@@ -39,27 +46,31 @@ instance FromJSON Event where
                             v .: "data"
      parseJSON _          = mzero
 
-sample = "{\"tag\": \"salt/job/20140203110645016162/new\", \"data\": {\"tgt_type\": \"glob\", \"jid\": \"20140203110645016162\", \"tgt\": \"salt\", \"_stamp\": \"2014-02-03_11:06:45.016495\", \"user\": \"vagrant\", \"arg\": [{\"module\": \"t\", \"__kwarg__\": true}], \"fun\": \"sys.argspec\", \"minions\": [\"salt\"]}}"
+-- lens getters are functions of Producers
+getLines::
+    Monad m
+    => Producer ByteString m ()
+    -> FreeT (Producer ByteString m) m ()
+getLines = view PB.lines
 
--- Removing "data: " by brute force for now
-jsonLowerBound :: Int
-jsonLowerBound = 6
-
-filters :: (MonadIO m) => Producer ByteString m () -> Producer Event m ()
-filters = loop . view PB.lines
+processEvtStream :: MonadIO m => Producer ByteString m () -> Producer Event m ()
+processEvtStream = go . getLines
   where
-    loop :: MonadIO m => FreeT (Producer ByteString m) m r -> Producer Event m r
-    loop freeT = do
-        x <- lift $ runFreeT freeT
+    go :: MonadIO m => FreeT (Producer ByteString m) m r -> Producer Event m r
+    go freeT = do
+        x <- lift $ runFreeT freeT -- runFreeT to get the next line (the next Producer inside the FreeT)
         case x of
-            Pure r -> return r
+            Pure r -> -- There aren't no group left for instance when the server stops streaming events
+                liftIO $ return r
             Free p -> do
-                (j, p') <- lift $ runStateT PAe.decode (p >-> PB.drop jsonLowerBound)
-                case j of
-                    Left  _    -> return () -- or deal with the error liftIO $ print e
-                    Right j' -> yield j'
+                (jr, p') <- lift $ runStateT PAe.decode (p >-> PB.drop jsonLowerBound)
+                case jr of
+                    Left  _    -> -- json parser returns an error
+                        return ()
+                    Right jv -> yield jv
+                -- p' :: Producer ByteString m (FreeT (Producer ByteString m) m r)
                 freeT' <- lift $ drain p'
-                loop freeT'
+                go freeT'
 
     drain :: Monad m => Producer a m r -> m r
     drain p = runEffect $ for p discard
@@ -68,4 +79,4 @@ main = do
     req <- parseUrl "http://localhost:8080/event/2b230e965c65eaf992962be7f0b0ab9b?tag=salt%2Fjob%2F"
     withManager defaultManagerSettings $ \m ->
        withHTTP req m $ \resp ->
-            runEffect $ for (filters (responseBody resp)) (liftIO.print)
+            runEffect $ for (processEvtStream (responseBody resp)) (liftIO.print)
