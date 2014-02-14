3473bc3febae45be84d77fbc422f3ca63c96110d
Pierre Radermecker <pradermecker@cirb.irisnet.be>
2014-02-14 14:44:45 +0100

    parsing time

--- saltevent.hs
+++ saltevent.hs
@@ -4,6 +4,9 @@
 module Main
 where
 
+import Data.Time.Format     (parseTime)
+--import System.Locale        (defaultTimeLocale)
+import Data.Time.Clock      (UTCTime)
 import           Control.Applicative
 import           Control.Lens        (view)
 import           Control.Monad
@@ -20,15 +23,19 @@ import qualified Pipes.ByteString    as PB
 import           Pipes.Group
 import           Pipes.HTTP
 import           Pipes.Parse
-import GHC.Int
+import System.Locale
 -- Removing "data: " by brute force for now
 jsonLowerBound :: Int
 jsonLowerBound = 6
 
-token = "2b230e965c65eaf992962be7f0b0ab9b"
+token = "edf5e44d8c12852af87d62423c21bf7f"
 serverUrl = "http://localhost:8080/event/" ++ token ++ "?tag=salt%2Fjob%2F"
+stampFormat = "%Y-%m-%d_%T%Q"
 
-insertSQL = "insert into events (jid, user, stamp, tgt, minions, fun, arg) values (?, ?, ?, ?, ?, ?, ?)"
+parseStampTime :: String -> Maybe UTCTime
+parseStampTime = parseTime defaultTimeLocale stampFormat
+
+insertSQL = "insert into event (jid, username, stamp, tgt, minions, fun, arg) values (?, ?, ?, ?, ?, ?, ?)"
 
 -- JSON PARSING
 data Event = Event
@@ -37,16 +44,17 @@ data Event = Event
     } deriving Show
 
 data Command = Command
+
     { jid       :: String
-    , user    :: String
-    , _stamp  :: String
+    , userName  :: String
+    , _stamp  :: Maybe UTCTime
     , tgt     :: String
     , minions :: [String]
     , fun     :: String
     , arg     :: [Value]
     } deriving (Show, Generic)
 
-instance FromJSON Command
+--instance FromJSON Command
 
 instance FromJSON Event where
      parseJSON (Object v) = Event <$>
@@ -54,8 +62,26 @@ instance FromJSON Event where
                             v .: "data"
      parseJSON _          = mzero
 
+instance FromJSON Command where
+     parseJSON (Object v) = Command <$>
+                            v .: "jid" <*>
+                            v .: "user" <*>
+                            liftM parseStampTime (v .: "_stamp") <*>
+                            v .:  "tgt" <*>
+                            v .: "minions" <*>
+                            v .:  "fun" <*>
+                            v .: "arg"
+     parseJSON _          = mzero
+
 instance Pg.ToRow Command where
-   toRow d = [toField (jid d), toField (user d), toField (_stamp d), toField (tgt d), toField $ PGArray (minions d), toField (fun d), toField $ PGArray (arg d)]
+   toRow d = [toField (jid d)
+             , toField (userName d)
+             , toField (_stamp d)
+             , toField (tgt d)
+             , toField $ PGArray (minions d)
+             , toField (fun d)
+             , toField $ PGArray (fmap toJSON (arg d))
+             ]
 
 -- lens getters are functions of Producers
 getLines::
@@ -63,11 +89,6 @@ getLines::
     => Producer ByteString m ()
     -> FreeT (Producer ByteString m) m ()
 getLines = view PB.lines
-
-hello :: Pg.ToRow q => q -> IO (Int64)
-hello q = do
-   conn <- Pg.connect Pg.defaultConnectInfo
-   Pg.execute conn "insert into users (first_name) values (?)" q
 
 processEvtStream :: MonadIO m => Pg.Connection -> Producer ByteString m () -> Producer Event m ()
 processEvtStream conn = go . getLines
@@ -81,10 +102,12 @@ processEvtStream conn = go . getLines
             Free p -> do
                 (jr, p') <- lift $ runStateT PAe.decode (p >-> PB.drop jsonLowerBound)
                 case jr of
-                    Left  _    -> -- json parser returns an error
+                    Left e    -> do -- json parser returns an error
+                        liftIO $ print e
                         return ()
                     Right jv -> do
                         let cmd = _data jv
+                        liftIO $ print cmd
                         liftIO $ Pg.execute conn insertSQL $ cmd
                         yield jv
                 -- p' :: Producer ByteString m (FreeT (Producer ByteString m) m r)
@@ -95,8 +118,8 @@ processEvtStream conn = go . getLines
     drain p = runEffect $ for p discard
 
 main = do
-    conn <- Pg.connect Pg.defaultConnectInfo { Pg.connectUser = "jules", Pg.connectDatabase = "jules" }
-    req <- parseUrl serverUrl
+    conn <- Pg.connect Pg.defaultConnectInfo { Pg.connectUser = "jules", Pg.connectDatabase = "jules", Pg.connectPassword = "jules"}
+    req <- parseUrl "http://localhost:8080/event/edf5e44d8c12852af87d62423c21bf7f?tag=salt%2Fjob%2F"
     withManager defaultManagerSettings $ \m ->
        withHTTP req m $ \resp ->
             runEffect $ for (processEvtStream conn (responseBody resp)) (liftIO.print)
--- setup-notes.md
+++ setup-notes.md
@@ -2,16 +2,19 @@ Database
 ========
 
 * sudo pacman -S postgres
+* [opt] change postgres user home to /home/postgres
+* [opt] add a .bash_profile with "export PGDATA = /var/lib/postgres/data"
+* [opt] you can use su - postgres to have a login shell
 * sudo -i -u postgres
 * (as postgres) initdb
 * (as postgres) create user --interactive
 * (as postgres) createdb -O jules jules
-* () create table event (
+* () CREATE TABLE event (
 		bigserial primary key,
-        jid  varchar(20) NOT NULL,
-        user varchar(20) NOT NULL,
-        _stamp varchar(20) NOT NULL,
-        tgt   varchar(20) NOT NULL,
-     	minions [varchar(30)],
-     	fun     varchar(20) NOT NULL,
-    	arg     [Int]
+        jid  text NOT NULL,
+        username text NOT NULL,
+        stamp timestamptz NOT NULL,
+        tgt   text NOT NULL,
+     	minions text[],
+     	fun     text NOT NULL,
+    	arg     integer[])
